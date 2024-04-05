d8c837c9cb41f8ca8e4e2075a51b66e45dcca5e8
riyagolani <riya.s.golani@gmail.com>
2024-04-05 16:12:08 -0400

    Adding profile endpoint to fetch user profile

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { User } from "../models/UserModel.js";
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
-
+const JWT_SECRET = crypto.randomBytes(64).toString('hex');
 
 // Create User POST Method
 router.post('/',async (request, response)=>{
@@ -43,6 +45,25 @@ router.post('/',async (request, response)=>{
 });
 
 
+// Authentication Middleware
+function authenticate(request, response, next) {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+        return response.status(401).send({ message: 'Unauthorized' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        request.user = { id: decoded.id };
+        next(); 
+    } catch (error) {
+        return response.status(401).send({ message: 'Invalid token' }); 
+    }
+}
+
+
 // User Login using username and password
 router.post('/login', async (request, response) => {
     try{
@@ -58,13 +79,42 @@ router.post('/login', async (request, response) => {
         if (!passwordMatch) {
             return response.status(401).send({ message: 'Invalid password' });
         }
+        const payload = {
+            id: user.id,
+            username: user.username,
+        }
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
         console.log(user);
-        return response.status(200).send(user); 
+        return response.status(200).send({token}); 
     }catch (error){
         console.log(error.message);
         return response.status(500).send({message: error.message});
     }
 });
 
+
+// User Login using username and password
+router.get('/profile', authenticate, async (request, response) => {
+    try {
+        // Retrieve user information (assume authentication middleware stores user ID on request.user)
+        const user = await User.findById(request.user.id);
+
+        if (!user) {
+            return response.status(404).send({ message: 'User not found' });
+        }
+        console.log("User found");
+        // Optionally filter sensitive information before sending response
+        const profileData = {
+            username: user.username,
+            email: user.email,
+            contact: user.contact,
+        };
+        console.log("Fetching user profile data");
+        return response.status(200).send(profileData); 
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({message: error.message});
+    }
+});
 
 export default router;
